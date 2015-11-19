c0e0fa759676d03c6ea7bd30fe577ca3574bc7c1
Joao Sallaberry <joaovictorhs@gmail.com>
2015-11-19 01:34:59 -0200

    graph as adjacency list

--- src/closeness_centrality/core.clj
+++ src/closeness_centrality/core.clj
@@ -4,32 +4,33 @@
 (use 'clojure.java.io)
 (require '[clojure.string :as str])
 
-(defn add-edge-from-line-helper [graph n1 n2]
-  (let [node1 (keyword (str \v n1))
-        node2 (read-string n2)
-        node-list (get graph node1)]
-    (if node-list
-      (assoc graph node1 (conj node-list node2))
-      (assoc graph node1 #{node2}))))
+(defn read-lines []
+  "Read lines from file"
+  (map (fn [line]
+         (str/split line #" "))
+       (str/split (slurp "resources/edges.txt") #"\r\n")))
 
-(defn add-edge-from-line [graph line]
-  (let [[node1 node2] (str/split line #" ")]
-    (add-edge-from-line-helper 
-     (add-edge-from-line-helper graph node1 node2) node2 node1)))
+(defn mirror-edges [lines]
+  (into
+   (map (fn [[n1 n2]]
+          (vector (keyword n1) (Integer. n2)))
+        lines)
+   (map (fn [[n1 n2]]
+          (vector (keyword n2) (Integer. n1)))
+        lines)))
 
-(defn read-graph []
-  "Read graph edges from file"
-  (with-open [rdr (reader "resources/edges.txt")]
-    (loop [line (line-seq rdr)
-           graph {}]
-      ;(println line)
-      (if (not (string? line))
-        graph
-        (recur (line-seq rdr) (add-edge-from-line graph line))))))
+(defn add-edges-to-graph [edges graph]
+  (reduce (fn [g [n1 n2]]
+            (if (contains? g n1)
+              (assoc g n1 (conj (n1 g) n2))
+              (assoc g n1 [n2])))
+          graph
+   edges))
+
+(def graph (add-edges-to-graph (mirror-edges (read-lines))
+                               (sorted-map)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (read-graph)
-  ;(bfs tree)
   )
