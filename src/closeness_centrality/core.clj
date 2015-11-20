6258a712546b338dd5ec91852b3af8b0effe8247
Joao Sallaberry <joaovictorhs@gmail.com>
2015-11-20 00:34:17 -0200

    BFS to find distance from one node to all others

--- src/closeness_centrality/core.clj
+++ src/closeness_centrality/core.clj
@@ -8,15 +8,15 @@
   "Read lines from file"
   (map (fn [line]
          (str/split line #" "))
-       (str/split (slurp "resources/edges.txt") #"\r\n")))
+       (str/split (slurp "resources/simple-test.txt") #"\r?\n")))
 
 (defn mirror-edges [lines]
   (into
    (map (fn [[n1 n2]]
-          (vector (keyword n1) (Integer. n2)))
+          (vector (keyword n1) (keyword n2)))
         lines)
    (map (fn [[n1 n2]]
-          (vector (keyword n2) (Integer. n1)))
+          (vector (keyword n2) (keyword n1)))
         lines)))
 
 (defn add-edges-to-graph [edges graph]
@@ -29,6 +29,24 @@
 
 (def graph (add-edges-to-graph (mirror-edges (read-lines))
                                (sorted-map)))
+
+(defn distance-to-all-nodes [g s]
+  "Runs BFS to find the distance from s to all other nodes"
+  (loop [distances {s 0}
+         explored #{s}
+         queue (conj (clojure.lang.PersistentQueue/EMPTY) s)]
+    (if (empty? queue)
+      distances
+      (let [v (peek queue)
+            neighbors (g v)
+            new-neighbors (remove explored neighbors)
+            node&dist #(hash-map % (inc (distances v)))]
+        (recur
+         (apply merge distances (map node&dist new-neighbors))
+         (into explored neighbors)
+         (into (pop queue) new-neighbors))))))
+
+
 
 (defn -main
   "I don't do a whole lot ... yet."
