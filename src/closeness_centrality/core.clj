8d993d257a761acbc3b8ebe7991bab7eae0e8aff
Joao Sallaberry <joaovictorhs@gmail.com>
2015-11-22 15:28:48 -0200

    sorting map by value is now generic
    Added docstring to all functions and make var names better

--- src/closeness_centrality/core.clj
+++ src/closeness_centrality/core.clj
@@ -8,26 +8,30 @@
 ;(def edges-file "resources/edges.txt")
 
 (defn read-lines [file]
-  "Read lines from file"
+  "Read lines of edges from file"
   (map (fn [line]
          (map keyword (str/split line #" ")))
        (str/split (slurp file) #"\r?\n")))
 
 (defn add-directed-edge [graph edge]
+  "Add directed edge to graph"
   (let [[n1 n2] edge]
     (if (contains? graph n1)
               (assoc graph n1 (conj (n1 graph) n2))
               (assoc graph n1 [n2]))))
 
 (defn add-edge [graph edge]
+  "Add edge to graph in both directions"
   (let [[n1 n2] edge]
     (add-directed-edge 
      (add-directed-edge graph [n1 n2]) [n2 n1])))
 
-(defn add-edges-from-lines [edges graph]
+(defn add-edges-to-graph [edges graph]
+  "Add a list of edges to graph"
   (reduce add-edge graph edges))
 
-(def graph (add-edges-from-lines (read-lines edges-file)
+(def graph "Graph generated from edges-file"
+  (add-edges-to-graph (read-lines edges-file)
                                  (sorted-map)))
 
 (defn rest-add-edge [n1 n2]
@@ -36,42 +40,38 @@
                               (keyword (str n2))]))
   {:message "done!"})
 
-(defn distance-to-all-nodes [g s]
-  "Runs BFS to find the distance from s to all other nodes"
+(defn distance-to-all-nodes [graph s]
+  "BFS to find the distance from s to all other nodes"
   (loop [distances {s 0}
          explored #{s}
          queue (conj (clojure.lang.PersistentQueue/EMPTY) s)]
     (if (empty? queue)
       distances
-      (let [v (peek queue)
-            neighbors (g v)
+      (let [n (peek queue)
+            neighbors (graph n)
             new-neighbors (remove explored neighbors)
-            node&dist #(hash-map % (inc (distances v)))]
+            node&dist #(hash-map % (inc (distances n)))]
         (recur
          (apply merge distances (map node&dist new-neighbors))
          (into explored neighbors)
          (into (pop queue) new-neighbors))))))
 
-(defn farness [g s]
-  (reduce-kv #(+ %1 %3) 0 (distance-to-all-nodes g s)))
+(defn farness [graph node]
+  "Farness value of node"
+  (reduce-kv #(+ %1 %3) 0 (distance-to-all-nodes graph node)))
 
-(defn closeness [g s]
-  (/ 1 (farness g s)))
+(defn closeness [graph node]
+  "Closeness value of node"
+  (/ 1 (farness graph node)))
 
-(defn nodes-closeness [g]
+(defn nodes-closeness [graph]
+  "Closeness value of each node in graph"
   (reduce-kv (fn [ranking key _]
-               (assoc ranking key (closeness g key)))
+               (assoc ranking key (closeness graph key)))
              (sorted-map)
-             g))
+             graph))
 
-(defn closeness-sorted-nodes [g]
-  (let [unsorted-nodes (nodes-closeness g)]
-    (into (sorted-map-by (fn [key1 key2]
-                           (compare [(unsorted-nodes key2) key2]
-                                    [(unsorted-nodes key1) key1])))
-          unsorted-nodes)))
-
-(def fraudulent #{})
+(def fraudulent "List of fraudulent nodes" #{:5})
 
 (defn rest-flag-fraudulent [graph node]
   "Endpoint for flagging nodes as fraudulent"
@@ -82,28 +82,39 @@
                      "' flagged as fraudulent")})
     {:message (str "node '"(name node) "' does not exist")}))
 
-(defn f-factor [dist]
+(defn f-factor [k]
+  "Find coefficient F(k) = (1 - (1/2)^k)"
   (let [exp #(reduce * (repeat %2 %1))]
-    (- 1 (exp 1/2 dist))))
+    (- 1 (exp 1/2 k))))
 
 (defn apply-factors-to-scores [scores factors]
+  "Multiply every value in scores by its correspondent in factors"
   (reduce-kv ; map??
    (fn [scores n score]
      (assoc scores n (* (scores n) score)))
    scores
    factors))
 
-(defn final-score [g]
+(defn final-score [graph]
+  "Final score of every node considering fraud"
   (reduce (fn [scores fraud]
             (apply-factors-to-scores
              scores
              (reduce-kv #(assoc %1 %2 (f-factor %3))
                         {}
-                        (distance-to-all-nodes g fraud))))
-          (nodes-closeness g)
+                        (distance-to-all-nodes graph fraud))))
+          (nodes-closeness graph)
           fraudulent))
+
+(defn sort-map-by-value [in-map]
+  "Sort a map by value"
+  (into (sorted-map-by (fn [key1 key2]
+                         (compare [(in-map key2) key2]
+                                  [(in-map key1) key1])))
+        in-map))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
+  (sort-map-by-value (final-score graph))
   )
